f72067d76de04ba51d3f3fb8d0d1a326c6c02980
HardikMody <hardik2205rocks@gmail.com>
2017-03-11 00:17:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article1', function (req, res) {
-  res.send('article 1 requested and sent');
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
